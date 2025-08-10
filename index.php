e8d85d5b31616c4830cf9cf8ae583d3c24d2997d
Isaac Navajas Pozo <51831724+isaacnavajaspozo@users.noreply.github.com>
2025-08-10 17:38:16 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Isaac Navajas Pozo</title>
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <style>
+        @font-face {
+            font-family: 'MiFuente';
+            src: url('SourceSerif4-Regular.ttf') format('truetype');
+            font-style: normal;
+        }
+
+        body {
+            font-family: 'MiFuente' !important;
+            background-color: #f5f5f5;
+            color: #212529;
+        }
+
+        h1 {
+            font-size: 60px;
+            font-weight: 900 !important;
+        }
+
+        .cita {
+            text-align: center;
+            text-align: right;
+            font-size: 20px;
+            font-weight: 100 !important;
+            font-style: italic;
+            padding-right: 170px;
+        }
+
+        .subrayado {
+            border-radius: 0px 25px 25px 25px;
+            background-color: #ffc107;
+        }
+
+        .subtitulo {
+            font-size: 12px;
+            color: #b3b3b3;
+            text-decoration: line-through;
+        }
+
+        .logo {
+            opacity: 0.3;
+            width: 100px;
+        }
+
+        .form-control:focus {
+            border-color: #ffe597 !important;
+            box-shadow: 0 0 0 .25rem rgba(253, 217, 13, 0.25);
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container mt-5 mb-5">
+        <div class="row">
+            <div class="col-12 p-3">
+                <h1>Códigos y matraces <br>para soluciones efectivas
+                </h1>
+                <p>Como alquimista digital, soy muy meticuloso con cada script que desarrollo.
+                    <br>Siempre busco las mejores pocimas para crear aplicaciones sólidas y que tengan un impacto real.
+                </p>
+
+                <a class="btn btn-warning subrayado" href="https://www.linkedin.com/in/isaac-navajas-pozo/"
+                    target="_blank" rel="noopener noreferrer">
+                    Sígueme en Linkedin
+                </a>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid bg-dark">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-white cita mt-5 mb-5 ">
+                    " La mente que se abre a una nueva idea <br>jamás volverá a su tamaño original. "
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container mt-5 mb-5">
+        <div class="row">
+            <div class="col-md-4 p-3">
+                <h2>👨‍🎓 Desarrollos WordPress</h2>
+                <p class="subtitulo">Precio de mercado: + 2.000€</p>
+            </div>
+            <div class="col-8 p-3">
+                <p>
+                    Es imposible ignorar a WordPress, con un 45% de todo Internet bajo este CMS, se ha
+                    convertido en una pieza clave de la web moderna. Sin embargo, es fácil caer en la tentación de
+                    desarrollar de cualquier manera utilizando maquetadores visuales de tipo drag & drop como Elementor
+                    o Divi. Estas herramientas, aunque rápidas, generan código duplicado, penalizan el SEO, incrementan
+                    los costes de mantenimiento y en definitiva, obligan a adaptarse a un ecosistema interminable de
+                    plugins, muchas veces de dudosa procedencia.
+                    <br><br>
+                    Por eso he creado Fukiya (un tema minimalista propio para WordPress), que me permite desarrollar
+                    directamente
+                    en PHP o incluso estructurar una nueva base de datos como si se tratara de un proyecto a medida. Al
+                    mismo tiempo, puedo aprovechar las funcionalidades nativas del CMS, como el sistema de plantillas de
+                    páginas o el dashboard, adaptándolo a las necesidades específicas de cada proyecto.
+                    <span class="subrayado">De la forma imperfecta al diseño impecable, transformo cada línea de código
+                        en una pieza única</span>.
+                    <br><br>
+                    Busco el equilibrio perfecto entre velocidad, diseño y optimización SEO.
+                    Combinando la lógica del desarrollo con la visión del proyecto y así convierto la idea en una obra
+                    que perdure.
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <hr class="container mt-5 mb-5">
+
+    <div class="container mt-5 mb-5">
+        <div class="row">
+            <div class="col-md-4 p-3">
+                <h2>⚗️ Desarrollos a medida</h2>
+                <p class="subtitulo">Precio de mercado: + 4.000€</p>
+            </div>
+            <div class="col-8 p-3">
+                <p>
+                    Aunque domino diversas artes ancestrales y lenguajes de programación como JavaScript y C, mi
+                    lenguaje predilecto es PHP.
+                    Con él, he creado un framework propio llamado iSNF (Systems Integration and New Features), diseñado
+                    para adaptarse tanto a proyectos grandes como pequeños. Este framework se fundamenta en sólidas
+                    prácticas de seguridad y cuenta con una extensa biblioteca de aplicaciones privadas.
+                    <br><br>
+                    Gracias a iSNF, puedo transformar cualquier idea en realidad, desarrollando desde ERP, CMS,
+                    eCommerce y LMS, hasta APIs, sistemas de autenticación, dashboards y mucho más. Como decían los
+                    antiguos alquimistas, <span class="subrayado">“la transición de la potencia al acto”</span>, es
+                    decir,
+                    convertir el estudio y
+                    trabajo preparatorio en resultados tangibles y efectivos.
+                    <br><br>
+                    Este enfoque me permite combinar creatividad, precisión y eficiencia para entregar soluciones
+                    robustas, escalables y adaptadas a las necesidades reales de cada proyecto.
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <hr class="container">
+
+    <div class="container mt-4 mb-5">
+
+        <div class="row text-center">
+            <div class="col-12 mt-5">
+                <h2>¿Quieres saber más sobre mí?</h2>
+            </div>
+            <div class="col-12 mb-4">
+                <p>¡Genial! Aquí puedes enviarme un mensaje plumífero y estaré encantado de responderte lo antes
+                    posible.</p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-4 p-3">
+                <img src="alquimia.jpg" alt="" class=" subrayado img-fluid img-col">
+            </div>
+            <div class="col-8 p-3">
+                <form action="https://formspree.io/f/xpwlklkz" method="POST">
+                    <div class="form-group">
+                        <label for="correo">Correo</label>
+                        <input type="email" class="form-control" id="correo" name="email">
+                    </div>
+                    <div class="form-group mt-4">
+                        <label for="texto">Mensaje</label>
+                        <textarea class="form-control" id="texto" rows="13" name="message"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-warning subrayado mt-4" style="width: 100%;">Enviar</button>
+                </form>
+
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12 mt-5" style="color:#d1d1d1;">
+                <center>
+                    <p>Desarrollado con <span
+                            style="opacity:0.4; filter: saturate(10%); display: inline-block;">🔥</span> por Isaac
+                        Navajas Pozo</p>
+                </center>
+            </div>
+        </div>
+
+    </div>
+
+</body>
+
+<script src="bootstrap.min.js"></script>
+
+</html>
